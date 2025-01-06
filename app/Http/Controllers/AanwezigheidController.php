343ba2ee8364588244ed4c48bf18f339e654fffe
Shahed Amer <chanyeol6521354@gmail.com>
2025-01-06 22:57:40 +0100

    een controller,model en migrations aangemaakt

---
+++ app/Http/Controllers/AanwezigheidController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\aanwezigheid;
+use Illuminate\Http\Request;
+
+class AanwezigheidController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(aanwezigheid $aanwezigheid)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(aanwezigheid $aanwezigheid)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, aanwezigheid $aanwezigheid)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(aanwezigheid $aanwezigheid)
+    {
+        //
+    }
+}
