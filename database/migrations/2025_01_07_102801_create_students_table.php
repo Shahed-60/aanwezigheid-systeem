cfc5d8ac8fa9e2819923e0e73bc6e13646f455f6
hazemafara <566508@edu.rocmn.nl>
2025-01-07 12:43:29 +0100

    tables and models created

--- database/migrations/2025_01_07_102801_create_students_table.php
+++ database/migrations/2025_01_07_102801_create_students_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('aanwezigheids', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('lastname');
+            $table->date('birthdate');
             $table->timestamps();
         });
     }
@@ -22,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('aanwezigheids');
+        Schema::dropIfExists('students');
     }
 };
