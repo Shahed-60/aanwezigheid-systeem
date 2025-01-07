97a2d42cdbb22f9bf95bb2c158c0217b0c4b0d88
nikan010 <9022105@student.zadkine.nl>
2025-01-07 14:53:04 +0100

    bezig met de insert

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
     public function StudentToevoegen(Request $request){
         //de producten in de database stoppen
-        // Student::instert([
-
-        // ])
+        Student::insert([
+            'id' => $request->id, 
+            'name' => $request->name,
+            'lastname' => $request->lastname,
+            'birthdate' => $request->birthdate,
+            'created_at' => Carbon::now()
+        ]);
     }
 }
